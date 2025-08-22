e0c4e0e3beb4ff4647580e5ea6a64f103b73d842
Torsten Weindl <torsten.weindl@gmail.com>
2025-08-22 12:40:15 +0200

    Saving current status

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ with col2:
     # """
     # st.write(output_text)
     st.markdown(f"Your taxi fare will approximately be: {fare} USD")
-    # st.markdown(f"### {fare} USD")
+    st.markdown(f"#### {fare} USD")
 
 col1, col2, col3, col4 = st.columns(4)
 
