a6245d4e934fdd6dcd2ffaaff77a994f46b83b3b
Torsten Weindl <torsten.weindl@gmail.com>
2025-08-22 14:59:11 +0200

    Saving current status

--- app.py
+++ app.py
@@ -28,48 +28,32 @@ with col1:
 with col2:
     pass
 
-col1, col2, col3, col4 = st.columns(4)
+# col1, col2, col3, col4 = st.columns(4)
 
-with col1:
+# with col1:
     # pickup_longitude = st.number_input('PICKUP longitude', value=-73.950655)
     # pickup_longitude = st.slider('PICKUP longitude', min_value=-74, max_value=-73, value=-73.950, step=0.001)
-    pickup_longitude = st.slider('PICKUP longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.950)
-with col2:
+    # pickup_longitude = st.slider('PICKUP longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.950)
+# with col2:
     # pickup_latitude = st.number_input('PICKUP latitude', value=40.783282)
-    pickup_latitude = st.slider('PICKUP latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.783282)
-with col3:
+    # pickup_latitude = st.slider('PICKUP latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.783282)
+# with col3:
     # dropoff_longitude = st.number_input('DROPOFF longitude', value=-73.984365)
-    dropoff_longitude = st.slider('DROPOFF longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.984365)
-with col4:
+    # dropoff_longitude = st.slider('DROPOFF longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.984365)
+# with col4:
     # dropoff_latitude = st.number_input('DROPOFF latitude', value=40.769802)
-    dropoff_latitude = st.slider('DROPOFF latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.769802)
+    # dropoff_latitude = st.slider('DROPOFF latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.769802)
 # passenger_count = st.number_input('Please tell me the number of passengers')
 
-params = {}
-params['pickup_datetime'] = date_time
-params['pickup_longitude'] = pickup_longitude
-params['pickup_latitude'] = pickup_latitude
-params['dropoff_longitude'] = dropoff_longitude
-params['dropoff_latitude'] = dropoff_latitude
-params['passenger_count'] = passenger_count
 
-response = requests.get(url, params=params)
-data = response.json()
-fare = round(float(data['fare']),2)
-# output_text = f"""
-#        Your taxi fare will approximately be:
-# """
-# st.write(output_text)
-st.markdown(f"Your taxi fare will approximately be: **{fare} USD**")
-# st.markdown(f"")
 
-map_data_dict = {
-    'lat': [pickup_latitude, dropoff_latitude],
-    'lon': [pickup_longitude, dropoff_longitude]
-}
-map_data = pd.DataFrame(map_data_dict)
-
-st.map(map_data)
+# map_data_dict = {
+#     'lat': [pickup_latitude, dropoff_latitude],
+#     'lon': [pickup_longitude, dropoff_longitude]
+# }
+# map_data = pd.DataFrame(map_data_dict)
+#
+# st.map(map_data)
 
 ###################
 
@@ -125,8 +109,21 @@ if output and 'last_clicked' in output:
             st.info("Beide Punkte wurden zurückgesetzt. Bitte wählen Sie jetzt Punkt B aus.")
             st.rerun()
 
-st.header("Gespeicherte Koordinaten")
-if st.session_state.point_a:
-    st.write(f"**Punkt A:** Latitude={st.session_state.point_a['lat']}, Longitude={st.session_state.point_a['lng']}")
-if st.session_state.point_b:
-    st.write(f"**Punkt B:** Latitude={st.session_state.point_b['lat']}, Longitude={st.session_state.point_b['lng']}")
+
+params = {}
+params['pickup_datetime'] = date_time
+params['pickup_longitude'] = st.session_state.point_a['lng']
+params['pickup_latitude'] = st.session_state.point_a['lat']
+params['dropoff_longitude'] = st.session_state.point_b['lng']
+params['dropoff_latitude'] = st.session_state.point_b['lat']
+params['passenger_count'] = passenger_count
+
+response = requests.get(url, params=params)
+data = response.json()
+fare = round(float(data['fare']),2)
+# output_text = f"""
+#        Your taxi fare will approximately be:
+# """
+# st.write(output_text)
+st.markdown(f"Your taxi fare will approximately be: **{fare} USD**")
+# st.markdown(f"")
