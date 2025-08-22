4fd0d57f45cbcd7c3b1f5758d0fb4ff5c4481b93
Torsten Weindl <torsten.weindl@gmail.com>
2025-08-22 12:35:57 +0200

    Saving current status

--- app.py
+++ app.py
@@ -20,23 +20,6 @@ with col2:
     time = st.time_input('Please tell me the pickup time:', datetime.time(8, 00))
     date_time = str(date) + " " + str(time)
 
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    # pickup_longitude = st.number_input('PICKUP longitude', value=-73.950655)
-    # pickup_longitude = st.slider('PICKUP longitude', min_value=-74, max_value=-73, value=-73.950, step=0.001)
-    pickup_longitude = st.slider('PICKUP longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.950)
-with col2:
-    # pickup_latitude = st.number_input('PICKUP latitude', value=40.783282)
-    pickup_latitude = st.slider('PICKUP latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.783282)
-with col3:
-    # dropoff_longitude = st.number_input('DROPOFF longitude', value=-73.984365)
-    dropoff_longitude = st.slider('DROPOFF longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.984365)
-with col4:
-    # dropoff_latitude = st.number_input('DROPOFF latitude', value=40.769802)
-    dropoff_latitude = st.slider('DROPOFF latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.769802)
-# passenger_count = st.number_input('Please tell me the number of passengers')
-
 col1, col2 = st.columns(2)
 with col1:
     passenger_count = st.number_input('# Passengers', value=1)
@@ -60,7 +43,22 @@ with col2:
     st.markdown(f"Your taxi fare will approximately be:")
     st.markdown(f"### {fare} USD")
 
+col1, col2, col3, col4 = st.columns(4)
 
+with col1:
+    # pickup_longitude = st.number_input('PICKUP longitude', value=-73.950655)
+    # pickup_longitude = st.slider('PICKUP longitude', min_value=-74, max_value=-73, value=-73.950, step=0.001)
+    pickup_longitude = st.slider('PICKUP longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.950)
+with col2:
+    # pickup_latitude = st.number_input('PICKUP latitude', value=40.783282)
+    pickup_latitude = st.slider('PICKUP latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.783282)
+with col3:
+    # dropoff_longitude = st.number_input('DROPOFF longitude', value=-73.984365)
+    dropoff_longitude = st.slider('DROPOFF longitude', min_value=-74.15, max_value=-73.79, step=0.000001, value=-73.984365)
+with col4:
+    # dropoff_latitude = st.number_input('DROPOFF latitude', value=40.769802)
+    dropoff_latitude = st.slider('DROPOFF latitude', min_value=40.58, max_value=40.78, step=0.000001, value=40.769802)
+# passenger_count = st.number_input('Please tell me the number of passengers')
 
 map_data_dict = {
     'lat': [pickup_latitude, dropoff_latitude],
